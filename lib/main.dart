84cb508284fa75c2e15fecb3730069c6dd7b4dae
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2025-07-17 06:30:09 +0300

    some minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,12 @@ void main() {
 GoRouter router = GoRouter(
   initialLocation: "/",
   routes: [
+    GoRoute(path: "/", builder: (context, state) => const RedirectPage()),
     GoRoute(
-      path: "/",
-      builder: (context, state) => const RedirectPage(),
-    ),
-    GoRoute(
-        path: "/login",
-        builder: (context, state) => const LoginPage(),
-        routes: [
-          /*GoRoute(
+      path: "/login",
+      builder: (context, state) => const LoginPage(),
+      routes: [
+        /*GoRoute(
             path: 'note/:noteId',
             builder: (BuildContext context, GoRouterState state) {
               return NoteScreen(note: notesBox.getNote(state.pathParameters["noteId"]!)!);
@@ -30,8 +27,8 @@ GoRouter router = GoRouter(
                 return SettingsScreen();
               }
           )*/
-        ]
-    )
+      ],
+    ),
   ],
 );
 
@@ -41,28 +38,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
-        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-          ColorScheme lightColorScheme = lightDynamic?.harmonized() ??
-              ColorScheme.fromSeed(seedColor: Colors.green);
-          ColorScheme darkColorScheme = darkDynamic?.harmonized() ??
-              ColorScheme.fromSeed(
-                  seedColor: Colors.green, brightness: Brightness.dark);
-          return MaterialApp.router(
-            routerConfig: router,
-            title: 'Beshence Account Manager',
-            theme: ThemeData(
-                colorScheme: lightColorScheme,
-                appBarTheme: AppBarTheme(backgroundColor: lightColorScheme.surface),
-                bottomAppBarTheme: BottomAppBarTheme(color: Colors.transparent)
-            ),
-            darkTheme: ThemeData(
-                colorScheme: darkColorScheme,
-                appBarTheme: AppBarTheme(backgroundColor: darkColorScheme.surface),
-                bottomAppBarTheme: BottomAppBarTheme(color: Colors.transparent)
-            ),
-            themeMode: ThemeMode.system,
-          );
-        });
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme =
+            lightDynamic?.harmonized() ??
+            ColorScheme.fromSeed(seedColor: Colors.green);
+        ColorScheme darkColorScheme =
+            darkDynamic?.harmonized() ??
+            ColorScheme.fromSeed(
+              seedColor: Colors.green,
+              brightness: Brightness.dark,
+            );
+        return MaterialApp.router(
+          routerConfig: router,
+          title: 'Beshence Account Manager',
+          theme: ThemeData(
+            colorScheme: lightColorScheme,
+            appBarTheme: AppBarTheme(backgroundColor: lightColorScheme.surface),
+            bottomAppBarTheme: BottomAppBarTheme(color: Colors.transparent),
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme,
+            appBarTheme: AppBarTheme(backgroundColor: darkColorScheme.surface),
+            bottomAppBarTheme: BottomAppBarTheme(color: Colors.transparent),
+          ),
+          themeMode: ThemeMode.system,
+        );
+      },
+    );
   }
 }
 
@@ -75,11 +77,8 @@ class RedirectPage extends StatelessWidget {
       context.replace('/login');
     });
 
-    return Scaffold(
-      body: Center(child: CircularProgressIndicator()),
-    );
+    return Scaffold(body: Center(child: CircularProgressIndicator()));
   }
-
 }
 
 class LoginPage extends StatefulWidget {
@@ -101,9 +100,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Beshence Account Manager"),
-      ),
+      appBar: AppBar(title: Text("Beshence Account Manager")),
       bottomNavigationBar: BottomAppBar(
         child: Row(
           children: [
@@ -115,57 +112,87 @@ class _LoginPageState extends State<LoginPage> {
           ],
         ),
       ),
-      body: Center(
-        child: SingleChildScrollView(
+      body: LayoutBuilder(
+        builder: (context, constraints) => SingleChildScrollView(
           padding: EdgeInsets.all(16),
-          child: Row(
-            children: [
-              Expanded(child: SizedBox.shrink()),
-              SizedBox(
-                width: 384,
-                child: Column(
-                  mainAxisAlignment: isLandscape(context) ? MainAxisAlignment.center : MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(Icons.account_circle_outlined, size: 48,),
-                    SizedBox(height: 24,),
-                    Text("Sign in", style: Theme.of(context).textTheme.headlineLarge?.copyWith(fontWeight: FontWeight.bold)),
-                    /*RichText(
-                      text: TextSpan(
-                        children: <TextSpan>[
-                          TextSpan(text: 'Sign in', style: Theme.of(context).textTheme.headlineLarge),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: constraints.maxHeight),
+            child: Row(
+              crossAxisAlignment: isLandscape(context)
+                  ? CrossAxisAlignment.center
+                  : CrossAxisAlignment.start,
+              children: [
+                Expanded(child: SizedBox.shrink()),
+                SizedBox(
+                  width: isLandscape(context)
+                      ? 384
+                      : (constraints.maxWidth - 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(Icons.account_circle_outlined, size: 48),
+                      SizedBox(height: 24),
+                      Text(
+                        "Sign in",
+                        style: Theme.of(context).textTheme.headlineLarge
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      /*RichText(
+                        text: TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(text: 'Sign in', style: Theme.of(context).textTheme.headlineLarge),
+                          ],
+                        ),
+                      ),*/
+                      SizedBox(height: 32),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Server address',
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Username',
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        ),
+                      ),
+                      SizedBox(height: 32),
+                      Row(
+                        children: [
+                          TextButton(
+                            onPressed: () => {},
+                            child: Text("Register"),
+                          ),
+                          Expanded(child: SizedBox.shrink()),
+                          SizedBox(width: 16),
+                          TextButton(
+                            onPressed: () => {},
+                            child: Text("Forgot password"),
+                          ),
+                          SizedBox(width: 16),
+                          FilledButton(
+                            onPressed: () => {},
+                            child: Text("Continue"),
+                          ),
                         ],
                       ),
-                    ),*/
-                    SizedBox(height: 32,),
-                    TextField(
-                      decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Server address'),
-                    ),
-                    SizedBox(height: 16,),
-                    TextField(
-                      decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Username'),
-                    ),
-                    SizedBox(height: 16,),
-                    TextField(
-                      obscureText: true, decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Password',),
-                    ),
-                    SizedBox(height: 32,),
-                    Row(
-                        children: [
-                          TextButton(onPressed: () => {}, child: Text("Register")),
-                          Expanded(child: SizedBox.shrink()),
-                          SizedBox(width: 16,),
-                          TextButton(onPressed: () => {}, child: Text("Forgot password")),
-                          SizedBox(width: 16,),
-                          FilledButton(onPressed: () => {}, child: Text("Continue")),
-                        ]
-                    ),
-                    SizedBox(height: 32,)
-                  ],
+                      SizedBox(height: 32),
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(child: SizedBox.shrink()),
-            ],
+                Expanded(child: SizedBox.shrink()),
+              ],
+            ),
           ),
         ),
       ),
@@ -173,5 +200,10 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-bool isPortrait(BuildContext context) {return MediaQuery.orientationOf(context) == Orientation.portrait;}
-bool isLandscape(BuildContext context) {return MediaQuery.orientationOf(context) == Orientation.landscape;}
+bool isPortrait(BuildContext context) {
+  return MediaQuery.orientationOf(context) == Orientation.portrait;
+}
+
+bool isLandscape(BuildContext context) {
+  return MediaQuery.orientationOf(context) == Orientation.landscape;
+}
